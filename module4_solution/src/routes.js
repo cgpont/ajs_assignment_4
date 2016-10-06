66d75a02ad6f643e7521dace1547ff2c244483df
Cesar Garcia Pont <cesar.garcia@hasbro.es>
2016-10-06 12:48:20 +0200

    Initial templates and basic router wiring

---
+++ module4_solution/src/routes.js
@@ -0,0 +1,43 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/templates/home.template.html'
+  })
+
+  // Categories page
+  .state('categories', {
+    url: '/categories',
+    templateUrl: 'src/templates/categories.template.html',
+    // controller: 'CategoriesController as categories',
+    // resolve: {
+    //   items: ['MenuDataService', function (MenuDataService) {
+    //     return MenuDataService.getAllCategories();
+    //   }]
+    // }
+  })
+
+  // Item detail
+  // .state('categories.itemDetail', {
+  //   url: '/item-detail/{itemId}',
+  //   templateUrl: 'src/templates/item-detail.template.html',
+  //   controller: 'ItemDetailController as itemDetail'
+  // });
+
+}
+
+})();
